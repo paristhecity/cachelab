abd3ed72e064d22b813e0da20de2184b59e323d0
Peter Maida <pmaida1357@gmail.com>
2017-12-10 22:59:39 -0500

    updated main, outlined runSim

--- csim.c
+++ csim.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <stdio.h>
 
+
+
 // Record the statistics of running the cache
 static int hit_count, miss_count, eviction_count;
 
@@ -15,21 +17,25 @@ static int hit_count, miss_count, eviction_count;
 
 struct line {
 	int validBit;
-	//tag???
-	int block; //blocks, pointer, mem address?
-	int blockSize; // B = 2**b
+	//tag??? **************************************?
+	int block;
+	int blockSize; // B = 2^b
 };
 
 struct set {
-	int s; //this set
+	int s; //this set ?????????????????????????????????????
 	struct line* myLines; 
 	int numLines; //E
 };
 
 struct cache {
 	struct set* mySets;
-	int numSets; //S = 2**s
+	int numSets; //S = 2^s
 };
+
+
+
+
 
 /*
  * printUsage - Print usage info
@@ -44,13 +50,14 @@ void printUsage()
     printf("  -E <num>   Number of lines per set.\n");
     printf("  -b <num>   Number of block offset bits.\n");
     printf("  -t <file>  Trace file.\n");
-    exit(0);
+    return;
 }
 
 
 
-/* printCache() - prints all of the data in the cache
-*/
+/* 
+ * printCache() - prints all of the data in the cache, keep it around for later :P
+ */
 void printCache(struct cache *myCache) {
 	for (int i = 0; i < myCache->numSets; i++) {
 		struct set  mySet = myCache->mySets[i];
@@ -65,34 +72,34 @@ void printCache(struct cache *myCache) {
 
 
 
-/* setCache() - sets the data in the cache to zero
+/* setCache() - sets the data in the cache blocks to zero, each are invalid, each size B...
+					(say S is number Sets, E is num Lines...)
 */
 void setCache(struct cache *myCache, int S, int E, int B) {
 
 	myCache->numSets = S;
 	myCache->mySets = malloc(sizeof(struct set) * S);
-	printf("%d, %d, %d\n", S, E, B);
-	// sets each set
+	
+	// sets each set within cache
 	for (int i = 0; i < S; i++) {
 	
 		struct set *mySet = (struct set*) malloc(sizeof(struct set));
 		mySet->numLines = E;
 		mySet->myLines = malloc(sizeof(struct line) * E);
 		
-		// sets each line
+		// sets each line within set
 		for (int j = 0; j < E; j++) {
 		
 			struct line *myLine = (struct line*) malloc(sizeof(struct line));
 			myLine->blockSize = B;
-			myLine->block = 0; // remove!!
+			myLine->block = 0;
 			myLine->validBit = 0;
 			mySet->myLines[j] = *myLine;
 		}
 		
 		myCache->mySets[i] = *mySet;
-		
+
 	}
-	
 }
 
 
@@ -116,10 +123,24 @@ int *getInputs(char* line, ssize_t len) {
 }
 
 
+
 /*
-*/
-int main(int argc, char *argv[])
-{
+ * runSim() - returns hit,miss,evict msg
+ 			- increments hit, misses, eviction counters accordingly
+ */
+char *runSim() {
+	return "hit/miss/eviction";
+	//return hit
+	//return miss
+	//return miss eviction
+}
+ 
+ 
+ 
+/*
+ *
+ */
+int main(int argc, char *argv[]) {
 	
 	// Set the command line input
 	int opt;
@@ -156,28 +177,29 @@ int main(int argc, char *argv[])
 	}
 	
 	if (optind >= argc) {
-		//printf("Seeking Argument, none found?!?!\n"); //weird? thing from opt man page
+		//printf("Seeking Argument, none found?!?!\n"); //weird? thing from opt man page!*!*!*!
 	}
 	
 	if (helpFlag) {
 		// print the usage of csim and exit the program successfully
 		printUsage();
-		return 0;
+		exit(0);
 	}
 	
 	
 	
-	
-	// set an empty cache
+	// reset the global counters for hits, misses, and evictions
 	hit_count = 0;
 	miss_count = 0;
 	eviction_count = 0;
+	
+	// allocate space for a cache
 	struct cache *myCache = (struct cache*) malloc(sizeof(struct cache));
 	setCache(myCache, 1<<s, E, 1<<b); // use S=2^s and B=2^b
 
 
 
-	
+	// check the tracefile
 	FILE *fptr;
 	if ((fptr = fopen(traceFile, "r")) == NULL) {
 		// quit the program if the tracefile could not be opened
@@ -186,6 +208,7 @@ int main(int argc, char *argv[])
 	}
 	
 	
+	// loops through each line of the file seperately
 	char *line;
 	size_t len = 0;
 	ssize_t read = 0;
@@ -201,40 +224,34 @@ int main(int argc, char *argv[])
 			// if the verbose flag is set, print the input line
 			line[read - 1] = '\0';
 			line++;
-			printf("%s", line);
+			printf("%s ", line);
 		}
 		
 		int *inputs= getInputs(line, read); // inputs is a malloced array that must be freed
 
-		printf("\t");
-		for (int i = 0; i < 3; i++) {
-			printf("*%d* ",inputs[i]); //print what the recieved inputs was!, remove
-		}
-		printf("\t");
 		
-		
+		// run the simulation and store the return message in status
+		char status[20] = "";
 		if ((char) inputs[0] == 'M'){
 			// if the instruction is a data modify
-			//runSim twice, each returns a 0,1,2 if hit miss or evict
-			printf("Run Simulation Twice");
+			strcat(status, runSim());
+			strcat(status, " ");
+			strcat(status, runSim()); // run the simulation again
 		} else { // (char) inputs[0] == 'L' || (char) inputs[0] == 'S'
 			// if the instruction is a data load or data store
-			//runSim once, returns print info
-			printf("Run Simulation Once");
+			strcat(status, runSim());
 		}
 		
 		if (verboseFlag) {
-			// if the user wants output printed
-			// print if there are any hits, misses, or evictions *****************************
-			printf("\n");
+			// if the user requested printed output
+			printf("%s\n", status);
 		}
 		
-		free(inputs); // done using the input line
-		
+		free(inputs); // finished using the input line
 		
 	}
 	
-	fclose(fptr);
+	fclose(fptr); // finished reading lines from the file
 	
     printSummary(hit_count, miss_count, eviction_count);
     return 0;
